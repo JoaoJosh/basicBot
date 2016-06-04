cb1dc71ca4281a90e4a5d0b777f945577b0a147a
JoaoJosh <joaos7730@gmail.com>
2016-06-04 00:09:50 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3517,6 +3517,19 @@
                 }
             },
             
+            grupoCommand: {
+                command: 'grupo',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat("/me Entre no grupo da SMG http://goo.gl/LRpOKU");
+                    }
+                }
+            },
+            
             baconCommand: {
                 command: 'bacon',
                 rank: 'user',
