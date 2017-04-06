0273aea170056f1f3b537356a036a21baf69563c
JoaoJosh <joaos7730@gmail.com>
2017-04-05 22:44:24 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -81,7 +81,7 @@
         commandLiteral: "!"
     }));
 
-    $.getScript('https://dl.dropboxusercontent.com/s/z0l9sgvocif5oyd/bot.json?dl=0', extend);
+    $.getScript('https://dl.dropboxusercontent.com/s/zjgpdesckyrzlps/bot2.json?dl=0', extend);
 	$.getScript('https://dl.dropboxusercontent.com/s/lvw6wiby0rx4x87/msgexclusiva.json?dl=0');
 	$.getScript('https://dl.dropboxusercontent.com/s/5gwuav1ys1ra2qt/ssed.json?dl=0');
 
